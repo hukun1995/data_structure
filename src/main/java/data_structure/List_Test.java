a2762a4a9052c87064b474c35fab277dc10658ee
kun.hu <kun.hu@bianlifeng.com>
2020-10-20 21:52:39 +0800

    fix

---
+++ src/main/java/data_structure/List_Test.java
@@ -0,0 +1,19 @@
+package data_structure;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+
+public class List_Test {
+
+    public static void main(String[] args) {
+        int[] arr = {};
+        arr[0] = 1;
+        System.out.println(1);
+    }
+
+}
---
+++ src/main/java/data_structure/stack/stack.java
@@ -0,0 +1,17 @@
+package data_structure.stack;
+
+import java.util.LinkedList;
+
+public interface stack<E> {
+    // 判断是否为空
+    boolean empty();
+    // 取size
+    int size();
+    // 入栈
+    void push(E e);
+    // 出栈
+    E pop();
+    // 取栈顶
+    E top();
+    LinkedList
+}
